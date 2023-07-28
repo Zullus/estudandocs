a936e8405f3b1b35464fc5a4169f18cda4069d13
Edson Santos <edson.santos@gmail.com>
2023-07-28 15:04:23 -0300

    Convertendo variáveis

--- variaaveis/Program.cs
+++ variaaveis/Program.cs
@@ -10,5 +10,28 @@ string email = "edson@xsinformatica.com.br";
 
 Console.WriteLine(email);
 
+var seunome = "Edson Santos"; 
 
+int ano = 2023;
+double ano2 = 2023;
 
+Console.WriteLine(ano2);
+
+decimal taxa = 12.98m;
+int taxa2 = (int)taxa;
+
+Console.WriteLine(taxa2);
+
+string taxa3 = taxa.ToString();
+Console.WriteLine(taxa3);
+
+DateTime data = DateTime.Now;
+string data2 = data.ToString();
+Console.WriteLine(data2);
+
+string resposta = "32";
+int idade2 = int.Parse(resposta);
+int idade3 = Convert.ToInt32(resposta);
+
+Console.WriteLine(idade2);
+Console.WriteLine(idade3);
