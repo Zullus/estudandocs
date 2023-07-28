39a1e5e0f7ae86ca24516d26616fd57f1afa34d4
Edson Santos <edson.santos@gmail.com>
2023-07-28 14:47:44 -0300

    Variaveis

---
+++ variaaveis/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int idade;
+decimal altura;
+string nome;
+
+idade = 49;
+nome = "Edson";
+
+string email = "edson@xsinformatica.com.br";
+
+Console.WriteLine(email);
+
+
+
