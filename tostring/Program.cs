647f6d5c1a53b98454e57b3a3500ff565b26ec51
Edson Santos <edson.santos@gmail.com>
2023-07-28 15:51:43 -0300

    Manipulando strings

---
+++ tostring/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var nome = "Edson";
+var sobrenome = "Santos";
+
+var nomeCompleto = nome + " " + sobrenome;
+var nomeCompleto2 = $"{nome} {sobrenome}";
+
+Console.WriteLine(nomeCompleto2);
+
+Console.WriteLine(nomeCompleto2.ToUpper());
+Console.WriteLine(nomeCompleto2.ToLower());
+
+Console.WriteLine(nomeCompleto2.Substring(5, 3));
+
+Console.WriteLine(nomeCompleto2.Contains('o'));
+Console.WriteLine(nomeCompleto2.Contains('z'));
+
+Console.WriteLine(nomeCompleto2.StartsWith('E'));
+Console.WriteLine(nomeCompleto2.EndsWith('s'));
+
+Console.WriteLine(nomeCompleto2.IndexOf('s'));
+Console.WriteLine(nomeCompleto2.LastIndexOf('s'));
