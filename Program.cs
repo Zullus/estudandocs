86e3addb113de4570c339ff1544946e8c7e52fc9
Edson Santos <edson.santos@gmail.com>
2023-07-28 14:16:05 -0300

    Primeiro programa C#

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá, Mundo!");
+Console.WriteLine("Digite seu nome");
+var nome = Console.ReadLine();
+Console.WriteLine("Olá, " + nome);
+Console.WriteLine("De novo");
+var recebido = Console.Read();
+Console.WriteLine(recebido);
